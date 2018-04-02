908aedf825262e6fb1025ccfa6e2f64d3c3a6213
Christian Barra <barrachri@gmail.com>
2018-04-02 23:13:05 +0200

    Fix linters.

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,9 +29,10 @@ from blueprints import routes as blueprint_routes
 from cargos import mCargo, mUser_cargos
 from cargos import routes as cargo_routes
 from config import Config as C
-from monitoring import mLog, routes as monitoring_routes
 from jobs import mJob
 from jobs import routes as job_routes
+from monitoring import mLog
+from monitoring import routes as monitoring_routes
 from probes import mProbe, mUser_probes
 from probes import routes as probe_routes
 from tests.fixtures import _validate_token
