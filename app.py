153c0de6e710d64970f448bfb01de3c548dd3194
Christian Barra <barrachri@gmail.com>
2018-04-02 22:58:11 +0200

    Put aiodocker back.

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ if __name__ == '__main__':
     host = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
     port = int(sys.argv[2]) if len(sys.argv) > 2 else 8080
     app = web.Application()
-    print(monitoring_routes)
     add_route(
         app,
         *api_routes,
